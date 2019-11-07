49019e2ca7ac84630b8c8cb39896b24dbc8ae39d
NikolaBrodic <nikola.brodic97@gmail.com>
2019-11-07 22:28:52 +0100

    Initial commit - Backend

---
+++ Backend/src/main/java/ftn/tim16/ClinicalCentreSystem/ClinicalCentreSystemApplication.java
@@ -0,0 +1,13 @@
+package ftn.tim16.ClinicalCentreSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicalCentreSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicalCentreSystemApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/ftn/tim16/ClinicalCentreSystem/ClinicalCentreSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.tim16.ClinicalCentreSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicalCentreSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
