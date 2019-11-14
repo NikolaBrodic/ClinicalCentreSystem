0f9a411f67952c5a8f1cfe165be69d5385b610c5
milanobrenovic <mmobrenovic@gmail.com>
2019-11-14 01:43:08 +0100

    Added patient handling and manipulation.

---
+++ Backend/src/main/java/ftn/tim16/ClinicalCentreSystem/resource/PatientResource.java
@@ -0,0 +1,53 @@
+package ftn.tim16.ClinicalCentreSystem.resource;
+
+import ftn.tim16.ClinicalCentreSystem.model.Patient;
+import ftn.tim16.ClinicalCentreSystem.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+public class PatientResource {
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @PostMapping("/patient")
+    public ResponseEntity<Void> createPatient(@RequestBody Patient patient) {
+        Patient createdPatient = this.patientRepository.save(patient);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(createdPatient.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @GetMapping("/patients/{id}")
+    public Patient getPatient(@PathVariable long id) {
+        return patientRepository.findById(id).get();
+    }
+
+    @GetMapping("/patients")
+    public List<Patient> getAllPatients() {
+        return patientRepository.findAll();
+    }
+
+    @PutMapping("/patients/{id}")
+    public ResponseEntity<Patient> updatePatient(@PathVariable long id, @RequestBody Patient patient) {
+        Patient patientUpdated = patientRepository.save(patient);
+        return new ResponseEntity<>(patientUpdated, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/patients/{id}")
+    public ResponseEntity<Void> deletePatient(@PathVariable long id) {
+        this.patientRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
