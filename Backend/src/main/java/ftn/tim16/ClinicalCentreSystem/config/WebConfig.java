73a38b78dea6c2938b7dc39a04aca2031353dafd
Jelena Popov <popov.jelena2@gmail.com>
2019-12-08 19:38:15 +0100

    Deleted unnecessary dependency and deserialization and serialization for date Time

--- Backend/src/main/java/ftn/tim16/ClinicalCentreSystem/config/WebConfig.java
+++ Backend/src/main/java/ftn/tim16/ClinicalCentreSystem/config/WebConfig.java
@@ -1,9 +1,6 @@
 package ftn.tim16.ClinicalCentreSystem.config;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,9 +13,5 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").
                 allowedOrigins("http://localhost:4200");
-        Jackson2ObjectMapperBuilder.json()
-                .featuresToDisable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE);
-        Jackson2ObjectMapperBuilder.json()
-                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 }
