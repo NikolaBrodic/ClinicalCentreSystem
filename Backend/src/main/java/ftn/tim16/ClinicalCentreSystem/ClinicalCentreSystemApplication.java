b13ee689115704133908b23688fdcadbef6fed2b
NikolaBrodic <nikola.brodic97@gmail.com>
2019-12-12 15:35:36 +0100

    Changed email sending to be asynchronous

--- Backend/src/main/java/ftn/tim16/ClinicalCentreSystem/ClinicalCentreSystemApplication.java
+++ Backend/src/main/java/ftn/tim16/ClinicalCentreSystem/ClinicalCentreSystemApplication.java
@@ -2,14 +2,16 @@ package ftn.tim16.ClinicalCentreSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableAsync
 public class ClinicalCentreSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClinicalCentreSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicalCentreSystemApplication.class, args);
+    }
 
 }
--- Backend/src/main/java/ftn/tim16/ClinicalCentreSystem/serviceimpl/EmailNotificationServiceImpl.java
+++ Backend/src/main/java/ftn/tim16/ClinicalCentreSystem/serviceimpl/EmailNotificationServiceImpl.java
@@ -2,22 +2,32 @@ package ftn.tim16.ClinicalCentreSystem.serviceimpl;
 
 import ftn.tim16.ClinicalCentreSystem.service.EmailNotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmailNotificationServiceImpl implements EmailNotificationService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    @Autowired
+    private Environment env;
+
     @Override
+    @Async
     public void sendEmail(String to, String subject, String text) {
         SimpleMailMessage mail = new SimpleMailMessage();
+        mail.setFrom(env.getProperty("spring.mail.username"));
         mail.setTo(to);
         mail.setSubject(subject);
         mail.setText(text);
 
         javaMailSender.send(mail);
+        System.out.println("Email sent at: " + LocalDateTime.now().toLocalTime());
     }
 }
