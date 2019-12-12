38e4d8da57458a89d36e5b651f06bdddc87c5fb0
NikolaBrodic <nikola.brodic97@gmail.com>
2019-12-12 12:56:07 +0100

    Email comosing is moved to a separeted method in every service

--- Backend/src/main/resources/data-postgres.sql
+++ Backend/src/main/resources/data-postgres.sql
@@ -14,22 +14,21 @@ insert into clinic (address, description, name,clinic_rating) values ('3.Oktobar
 insert into clinic (address, description, name,clinic_rating) values ('Marsala Tita 9c', 'Savremena klinika u Novom Sadu', 'Klinika mira',0);
 
 /*CLINIC ADMIN*/
-/* Password: Admin.04 */
- insert into clinic_administrator (email, password, first_name, last_name, phone_number, status,clinic_id) values
-('2st.admin@maildrop.cc', '$2a$10$0fX3yNqeWX3O/f0HtZRtVOA/6SA1Jq2vqARolGgJjRu47FX4AE/Y.', 'Mark1o', 'Marković1', '064153456', 'ACTIVE',1);
+/* Password: adminC.0 */
+insert into clinic_administrator (email, password, first_name, last_name, phone_number, status,clinic_id) values
+ ('admin1@maildrop.cc', '$2a$10$VGG0KuDEe.M7Q8x9hWsXEuWWGaLGz9cDHjTa6bXyOXr25VzIP/sIO', 'Mark1o', 'Marković1', '064153456', 'ACTIVE',1);
 insert into clinic_admin_authority(user_id, authority_id) values (1,2);
 
-/* Password: Admin.02 */
 insert into clinic_administrator (email, password, first_name, last_name, phone_number, status,clinic_id) values
- ('example2@maildrop.cc', '$2a$10$tSSrg2pGJrulxcsYIPfQkusLZfwDKslf76oP8at5BQPbaAQl4Ixpq', 'Mario', 'Simic', '0652561980', 'ACTIVE',1);
+ ('admin2@maildrop.cc', '$2a$10$VGG0KuDEe.M7Q8x9hWsXEuWWGaLGz9cDHjTa6bXyOXr25VzIP/sIO', 'Mario', 'Simic', '0652561980', 'ACTIVE',1);
 insert into clinic_admin_authority(user_id, authority_id) values (2,2);
-/* Password: Admin.03 */
+
 insert into clinic_administrator (email, password, first_name, last_name, phone_number, status,clinic_id) values
-('example3@maildrop.cc', '$2a$10$hFfDWwXv6Ad6CS/eHyhVuefHgWGdYiJpTmfelv7JKhohKBuayn15K', 'Petar', 'Peric', '064123276', 'ACTIVE',1);
+('admin3@maildrop.cc', '$2a$10$VGG0KuDEe.M7Q8x9hWsXEuWWGaLGz9cDHjTa6bXyOXr25VzIP/sIO', 'Petar', 'Peric', '064123276', 'ACTIVE',1);
 insert into clinic_admin_authority(user_id, authority_id) values (3,2);
-/* Password: Admin.04 */
+
 insert into clinic_administrator (email, password, first_name, last_name, phone_number, status,clinic_id) values
- ('example4@maildrop.cc', '$2a$10$/.0cmhd2AYDGBQHGJn2RPel.sOUmjTe1f7DCUyGwW31NBh0bollBS', 'Aleksa', 'Aleksic', '0652539280', 'ACTIVE',2);
+ ('admin4@maildrop.cc', '$2a$10$VGG0KuDEe.M7Q8x9hWsXEuWWGaLGz9cDHjTa6bXyOXr25VzIP/sIO', 'Aleksa', 'Aleksic', '0652539280', 'ACTIVE',2);
 insert into clinic_admin_authority(user_id, authority_id) values (4,2);
 
 /*EXAMINATION TYPE*/
