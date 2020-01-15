96476ad9fadb930b2f74754bbec98b8aad71462d
Jelena Popov <popov.jelena2@gmail.com>
2020-01-15 20:30:45 +0100

    Made end-to-end tests for approving and rejecting request for time off/holiday for repository and service

--- Backend/src/test/java/ftn/tim16/ClinicalCentreSystem/TestSuite.java
+++ Backend/src/test/java/ftn/tim16/ClinicalCentreSystem/TestSuite.java
@@ -4,9 +4,7 @@ import ftn.tim16.ClinicalCentreSystem.controller.TimeOffDoctorControllerIntegrat
 import ftn.tim16.ClinicalCentreSystem.controller.TimeOffNurseControllerntegrationTests;
 import ftn.tim16.ClinicalCentreSystem.repository.TimeOffDoctorRepositoryIntegrationTests;
 import ftn.tim16.ClinicalCentreSystem.repository.TimeOffNurseRepositoryIntegrationTests;
-import ftn.tim16.ClinicalCentreSystem.service.TimeOffDoctorServiceIntegrationTests;
 import ftn.tim16.ClinicalCentreSystem.service.TimeOffDoctorServiceUnitTests;
-import ftn.tim16.ClinicalCentreSystem.service.TimeOffNurseServiceIntegrationTests;
 import ftn.tim16.ClinicalCentreSystem.service.TimeOffNurseServiceUnitTests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -17,8 +15,6 @@ import org.junit.runners.Suite;
         TimeOffNurseRepositoryIntegrationTests.class,
         TimeOffDoctorServiceUnitTests.class,
         TimeOffNurseServiceUnitTests.class,
-        TimeOffDoctorServiceIntegrationTests.class,
-        TimeOffNurseServiceIntegrationTests.class,
         TimeOffDoctorControllerIntegrationTests.class,
         TimeOffNurseControllerntegrationTests.class
 })
