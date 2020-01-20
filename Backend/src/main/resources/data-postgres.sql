99271afd44c6a3dfb70faa2df37287e39c82d61e
Jelena Popov <popov.jelena2@gmail.com>
2020-01-20 09:21:26 +0100

    Made database for room controller E2E tests

--- Backend/src/main/resources/data-postgres.sql
+++ Backend/src/main/resources/data-postgres.sql
@@ -21,11 +21,11 @@ insert into examination_type (label, price,status,clinic_id,version) values ('Zu
 insert into doctor_authority(user_id, authority_id) values (1,4);
 
  insert into doctor (email, password, first_name, last_name, phone_number,work_hours_from,work_hours_to,clinic_id,status,specialized_id,doctor_rating)
- values ('ClinicDoctor2@maildrop.cc', '$2a$10$0X7DgfIVxh1.MJoKeIp6juCEkrfYplqYrnUI7oB/LyElZ7dSZ2Z1a', 'Miodrag', 'Lazarevic', '065257165','00:01','07:30',1,'ACTIVE',1,2.5);
+ values ('ClinicDoctor2@maildrop.cc', '$2a$10$0X7DgfIVxh1.MJoKeIp6juCEkrfYplqYrnUI7oB/LyElZ7dSZ2Z1a', 'Miodrag', 'Lazarevic', '065257165','06:01','23:30',1,'ACTIVE',2,2.5);
 insert into doctor_authority(user_id, authority_id) values (2,4);
 
  insert into doctor (email, password, first_name, last_name, phone_number,work_hours_from,work_hours_to,clinic_id,status,specialized_id, doctor_rating)
- values ('ClinicDoctor3@maildrop.cc', '$2a$10$E1K7MZ9u5TkTZtESLfc8lOulf9/aPXKXhsFxtZQYMAeRnk9ezZ42q', 'Misa', 'Popovic', '065356856','07:30','20:00',1,'ACTIVE',1,4);
+ values ('ClinicDoctor3@maildrop.cc', '$2a$10$E1K7MZ9u5TkTZtESLfc8lOulf9/aPXKXhsFxtZQYMAeRnk9ezZ42q', 'Misa', 'Popovic', '065356856','07:30','20:00',1,'ACTIVE',2,4);
 insert into doctor_authority(user_id, authority_id) values (3,4);
 
 insert into nurse (email, password, first_name, last_name, phone_number,work_hours_from,work_hours_to,clinic_id,status)
@@ -33,11 +33,11 @@ values ('Nurse.01@maildrop.cc', '$2a$10$dT.7xoRoI338DdVr0E19EOj4/xrYSFyRLF6CAWph
 insert into nurse_authority(user_id, authority_id) values (1,5);
 
 insert into nurse (email, password, first_name, last_name, phone_number,work_hours_from,work_hours_to,clinic_id,status)
-values ('Nurse.02@maildrop.cc', '$2a$10$dT.7xoRoI338DdVr0E19EOj4/xrYSFyRLF6CAWphlBEONNnX22WfK', 'Tamara', 'Simic', '065432433','00:01','07:30',1,'ACTIVE');
+values ('Nurse.02@maildrop.cc', '$2a$10$dT.7xoRoI338DdVr0E19EOj4/xrYSFyRLF6CAWphlBEONNnX22WfK', 'Tamara', 'Simic', '065432433','02:01','23:30',1,'ACTIVE');
 insert into nurse_authority(user_id, authority_id) values (2,5);
 
 insert into nurse (email, password, first_name, last_name, phone_number,work_hours_from,work_hours_to,clinic_id,status)
-values ('Nurse.03@maildrop.cc', '$2a$10$dT.7xoRoI338DdVr0E19EOj4/xrYSFyRLF6CAWphlBEONNnX22WfK', 'Jelena', 'Simic', '075432433','00:01','07:30',1,'ACTIVE');
+values ('Nurse.03@maildrop.cc', '$2a$10$dT.7xoRoI338DdVr0E19EOj4/xrYSFyRLF6CAWphlBEONNnX22WfK', 'Jelena', 'Simic', '075432433','07:01','23:30',1,'ACTIVE');
 insert into nurse_authority(user_id, authority_id) values (3,5);
 
 insert into date_time_interval (start_date_time, end_date_time) values ( '07.05.2020 08:00','15.05.2020 10:00:00');
@@ -73,3 +73,40 @@ insert into patient (email, password, first_name, last_name, phone_number,addres
  values ('Patient3@maildrop.cc', '$2a$10$ItcRjGVnH26jIqw7GRXOBeM5Wu3sGxkSdvSgTRWSjEpPno4J9T4kS', 'Srdjan', 'Stanojevic', '065898255','Marsala tita 17','Kikinda',
  'Srbija','0625351286815','ACTIVATED',0);
 insert into patient_authority(user_id, authority_id) values (1,3);
+
+insert into date_time_interval (start_date_time, end_date_time) values ('20.06.2021 09:30:00', '20.06.2021 10:30:00');
+insert into examination (kind,interval_id,status,examination_type_id,clinic_id,clinic_administrator_id,patient_id,room_id,nurse_id,version)
+ values ('EXAMINATION',3,'APPROVED',1,1,1,3,1,1,0);
+insert into examining (examination_id,doctor_id) values (1,1);
+
+insert into date_time_interval (start_date_time, end_date_time) values ('20.06.2021 09:45:00','20.02.2021 10:45:00');
+insert into examination (kind,interval_id,status,examination_type_id,clinic_id,clinic_administrator_id,version)
+ values ('EXAMINATION',4,'AWAITING',2,1,3,0);
+insert into examining (examination_id,doctor_id) values (2,2);
+
+ insert into date_time_interval (start_date_time, end_date_time) values ('20.06.2021 10:35:00', '20.06.2021 11:45:00');
+insert into examination (kind,interval_id,status,examination_type_id,clinic_id,clinic_administrator_id,patient_id,room_id,nurse_id,version)
+ values ('EXAMINATION',5,'APPROVED',2,1,1,3,2,2,0);
+insert into examining (examination_id,doctor_id) values (3,2);
+
+
+insert into date_time_interval (start_date_time, end_date_time) values ('21.07.2021 10:30:00', '21.07.2021 12:30:00');
+insert into examination (kind,interval_id,status,examination_type_id,clinic_id,clinic_administrator_id,patient_id,room_id,nurse_id,version)
+ values ('EXAMINATION',3,'APPROVED',1,1,1,3,1,1,0);
+insert into examining (examination_id,doctor_id) values (4,1);
+
+insert into date_time_interval (start_date_time, end_date_time) values ('21.07.2021 11:00:00', '21.07.2021 13:30:00');
+insert into examination (kind,interval_id,status,examination_type_id,clinic_id,clinic_administrator_id,version)
+ values ('EXAMINATION',4,'AWAITING',2,1,3,0);
+insert into examining (examination_id,doctor_id) values (5,2);
+
+ insert into date_time_interval (start_date_time, end_date_time) values ('21.07.2021 12:45:00', '21.07.2021 13:45:00');
+insert into examination (kind,interval_id,status,examination_type_id,clinic_id,clinic_administrator_id,patient_id,room_id,nurse_id,version)
+ values ('EXAMINATION',5,'APPROVED',1,1,1,3,2,2,0);
+insert into examining (examination_id,doctor_id) values (6,1);
+
+
+insert into date_time_interval (start_date_time, end_date_time) values ('21.08.2021 12:45:00', '21.08.2021 13:45:00');
+insert into examination (kind,interval_id,status,examination_type_id,clinic_id,clinic_administrator_id,version)
+ values ('EXAMINATION',9,'AWAITING',2,2,2,0);
+insert into examining (examination_id,doctor_id) values (6,1);
